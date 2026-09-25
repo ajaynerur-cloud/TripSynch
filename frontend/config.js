46f90706422654dd3c256a1b2ff1ca4a1cc87a4d
ajaynerur-cloud <ajaynerur@gmail.com>
2026-09-25 22:03:44 +0530

    Add APP_URL to TRIPSYNCH_CONFIG

--- frontend/config.js
+++ frontend/config.js
@@ -1,1 +1,4 @@
-window.TRIPSYNCH_CONFIG={API_BASE:"https://tripsynch.onrender.com"};
+window.TRIPSYNCH_CONFIG = {
+  API_BASE: "https://tripsynch.onrender.com",
+  APP_URL: "https://ajaynerur-cloud.github.io/TripSynch"
+};
